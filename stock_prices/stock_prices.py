c8e3b837798bc13154a09a7bbdfc19e503974772
Luis Mendes <cvlopes88@gmail.com>
2020-02-20 16:35:20 -0500

    done with stock_prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,8 +3,22 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  max_profit = float('-inf')
+  min_price = float('inf')
+  
+  for i in range(len(prices) -1):
+    if prices[i] < min_price:
+      min_price = prices[i]
+      #now compare the price against all remaining
+      for j in range(i + 1, len(prices) - 1):
+        profit = prices[j] - prices[i]
+        # compare against profit
+        if profit > max_profit:
+          max_profit = profit
+          
+  return max_profit
 
+  
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
